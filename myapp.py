ce3e866431e55c226587680f76ac69783f68a7db
Abdul Mutalib <trust_dboy@yahoo.com>
2017-07-17 10:44:11 +0000

    i have modified the existing codes

--- myapp.py
+++ myapp.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, send_from_directory
 
-app	= Flask(__name__)
+app     = Flask(__name__)
 
 
 @app.route('/image/<path:path>')
@@ -10,12 +10,24 @@ def send_js(path):
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+        return render_template('index.html')
+
 
 @app.route('/whereami')
-def	whereami():
-	return "kdua"
+def     whereami():
+        return "kdua"
+
+
+@app.route('/hello/<name>')
+def	foo(name):
+	return render_template('index.html', to=name)
+
+@app.route('/python')
+def	dboy():
+	return render_template('python.html')
+
 
 if __name__=='__main__':
-	app.run(host="0.0.0.0")
+        app.run(host="0.0.0.0")
+
 
