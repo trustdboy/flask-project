effc239b70e4ed25940913e1e0f04442e6b1562a
trustdboy <trustdboy@yahoo.com>
2017-07-19 14:58:50 +0000

    changes_made

--- myapp.py
+++ myapp.py
@@ -1,7 +1,26 @@
 from flask import Flask, render_template, send_from_directory
 
+import os
+
+import paho.mqtt.client as mqtt
+
+client = mqtt.Client()
+client.connect("moorhouseassociates.com" , 1883,60)
+
+
+
 app     = Flask(__name__)
 
+
+@app.route('/')
+def index():
+	return render_template("index.html" , to="Sam Moorhouse"
+
+@app.route('/btn')
+def	btn():
+	print("button clicked")
+	client.publish("test/all", " Trust is walking along the line ")
+	return ""
 
 @app.route('/image/<path:path>')
 def send_js(path):
